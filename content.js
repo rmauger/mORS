a007a12740cd7c79d3a43a87aa0fa68bc7fa8626
Bobby Mauger <bobby.mauger@gmail.com>
2021-10-05 00:28:46 -0700

    findTOC is working

--- content.js
+++ content.js
@@ -4,7 +4,7 @@ document.addEventListener("load", myFunction())
 function myFunction (){
 //	window.alert("I'm running.");
 	let count = 0;
-	const aTab = /
+	const aTab = /\&nbsp\;/
 	const findORS = /((?<!(\W|\d){2,})\d{1,3}[A-C]?\.\d{3})/g;
 	const findSN = /((19|20)(\d{2}))\sc\.(\d{1,4})/g;
 	const findTitle = /(?<=\>)(([A-Z0-9]|\s){5,})/g;
@@ -20,7 +20,7 @@ function myFunction (){
 			para.innerHTML = para.innerHTML.replace(findSN, replaceSN);
 			
  	});
-	const findTOC = /((\d{1,3}\.\d{3})(.|\n)+)(?=(\&nbsp\;){2}\2)/;
+	const findTOC = /((\d{1,3}\.\d{3})(.|\n)+)(?=(&nbsp;){2}\s?\2)/;
 	const replaceTOC = '<div style="background-color:cyan">$1</div>';
 	document.body.innerHTML=document.body.innerHTML.replace(findTOC, replaceTOC)
 	console.log('Ran on ' + count +' paragraphs')
