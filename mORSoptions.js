c4926ab5bd64ce5d9a5440cd912d794ec3a46bc6
Bobby Mauger <bobby.mauger@gmail.com>
2021-10-08 02:40:12 -0700

    Messing with form (without much luck)

--- mORSoptions.js
+++ mORSoptions.js
@@ -1,22 +1,39 @@
-
-DisplayUserOptions();
-document.getElementById('optionsSubmit').onclick = SaveUserOptions();
-
-function SaveUserOptions() {
-  SavedDataObj.formDark = document.getElementById('dark').value();
-  let SavedUserData.doesExist = True;
-  let SavedUserData.formHein = document.getElementById('Hein').value();
-  let SavedUserData.formOrLeg = document.getElementById('OrLeg').value();
-  let SavedUserData.formNone = document.getElementById('None').value();
-  chrome.storage.local.set({'mORSoptions': SavedData});
-  
-//  chrome.tabs.executeScript(null, {
-//    file: 'content_script.js'
+"use strict"
+displayUserOptions();
+document.getElementById('optionsSubmit').onclick = function () {
+  let orLaws="";
+  const radioButtons = document.querySelectorAll('input[name="orLaws"]');
+  for (const choice of radioButtons){
+    if (choice.checked) {
+      const orLaws = choice.value;
+    }
+  }
+  let UserData = {
+    doesExist: true,
+    isDark: (document.getElementById('isDark').checked),
+    sourceOrLaws: orLaws
+  }
+  chrome.storage.sync.set({'mORSoptions': UserData});
 };
 
-
 function displayUserOptions(){
-   chrome.storeage.local.get(['storedUserOptions', SavedData])
-   if (SavedData.exists)
-   
+  
+  chrome.storage.sync.get(['mORSoptions'], function(object) {
+  let SavedData = object.mORSoptions || []; 
+    if (SavedData.doesExist){
+      alert (SavedData.sourceOrLaws + SavedData.doesExist + SavedData.isDark);
+      switch (SavedData.sourceOrLaws) {
+        case "Hein":
+          document.getElementById('Hein').checked=true;
+        break;
+        case "OrLeg": 
+          document.getElementById('OrLeg').checked=true;
+        break
+        default:
+          document.getElementById('None').checked=true; 
+        break;
+      };
+      document.getElementById('isDark').checked=SavedData.isDark
+    }; 
+  });  
 }
