f5d7b0038c931b2d0946fee9a064873e4535a5ff
Bobby Mauger <bobby.mauger@gmail.com>
2021-10-10 02:22:25 -0700

    Finally have storage piece working

--- background.js
+++ background.js
@@ -6,18 +6,19 @@ chrome.storage.onChanged.addListener(function (changes, namespace) {
     var myVal = Object.entries[keys[i]];
     alert(myVal);
   }
-}); */
-var myA="";
+}); 
+
 chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+  var myA="";
   const myQ = request.bgQuery;
     switch (myQ) {
     case "isDark":
-      chrome.storage.sync.get(['isDarkStored'], function(object) {
+      chrome.storage.sync.get('isDarkStored', (object) => {
         myA = object.isDarkStored||false
       });
       break;
     case "lawsReader":
-      chrome.storage.sync.get(['lawsReaderStored'], function(object) {
+      chrome.storage.sync.get('lawsReaderStored', function(object) {
         myA = object.lawsReaderStored||"flagFalse"
       });
       break;
@@ -26,6 +27,11 @@ chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
       break;
     };
   sendResponse({ans: myA});
-  alert("Sending " + myA)
+  console.log(`Sending in response to *${myQ}* answer: *${myA}*`);
   return true;
 });
+
+function getDark() {
+  return chrome.storage.sync.get('isDarkStored', function(object) {return object.isDarkStored || false})
+}
+*/
--- popup.js
+++ popup.js
@@ -1,34 +1,36 @@
 //popup.js
 
 "use strict";
+
+let promiseGetDark = new Promise((resolve) => {
+  chrome.storage.sync.get('isDarkStored', (object) => {
+    if (object) {
+      resolve(object.isDarkStored)
+    }
+  });
+});
+let promiseGetOrLaw = new Promise((resolve) => {
+  chrome.storage.sync.get('lawsReaderStored', (object) => {
+    if (object) {
+      resolve(object.lawsReaderStored)
+    }
+  });
+});
 displayUserOptions();
 
-function displayUserOptions() { 
-  
-  chrome.runtime.sendMessage({bgQuery:"isDark"}, function (response) {
-    let isDark=response.ans;
-    document.getElementById("isDark").checked=isDark;
-  });
-
-  chrome.runtime.sendMessage({bgQuery:"lawsReader"}, function(response) {
-    let lawsReader= response.ans;
-    alert("Asker received re:Reader: " + lawsReader)
-    //document.getElementById(lawsReader).checked=true;
-  })
-
-//  alert (`Retrieved: is dark =${document.getElementById("isDark").checked}`);
-    
+function displayUserOptions() {
+  promiseGetDark.then((resolve) => {document.getElementById("isDark").checked = resolve});
+  promiseGetOrLaw.then((resolve) => {document.getElementById(resolve).checked=true;})
 };
 
 document.getElementById('optionsSubmit').onclick = function () {
-  chrome.storage.sync.set({'isDarkStored': document.getElementById("isDark").checked}) //, function( {chrome.tabs.executeScript({file: "content_script3.js})})
+  chrome.storage.sync.set({'isDarkStored': document.getElementById("isDark").checked}) 
   let orLaws="";
-  const orLawRadios = document.querySelectorAll('input[name="orLaws"]'); //pulls data from OrLaw source radio buttons on website
+  const orLawRadios = document.querySelectorAll('input[name="orLaws"]');
   for (const choice of orLawRadios){
     if (choice.checked) {
       chrome.storage.sync.set({'lawsReaderStored': choice.id});
       orLaws=choice.id;
     };
   };
-  // defintely working: alert(`Saved: LawsReader = ${orLaws}  Is dark = ${document.getElementById("isDark").checked}`)
 };
