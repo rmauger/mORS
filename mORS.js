af389461df6d4c8a5ae35403c292ebc4a4ccb3a6
RobertAtLaw <bobby.mauger@gmail.com>
2021-10-12 15:35:34 -0700

    sped up ORS out of chapter linking

--- mORS.js
+++ mORS.js
@@ -4,8 +4,8 @@ window.addEventListener("load", StyleSheetRefresh);
 function ReplaceText(){
 	//common variables:
 	const tabs = "(?:&nbsp;|\\s){0,8}";
-	let temp=document.body.innerHTML;
-	const chapNum = temp.match(/(?<=Chapter\s)(\d{1,3}[A-C]?)/)[1];
+	let chapHTML=document.body.innerHTML;
+	const chapNum = chapHTML.match(/(?<=Chapter\s)(\d{1,3}[A-C]?)/)[1];
 
 	const doubleP = /<(\w)[^>]*?>(?:&nbsp;|\s)*<\/\1>/g; // is deleted (used in FinalClean also)
 	HTMLCleanUp();
@@ -14,12 +14,12 @@ function ReplaceText(){
 		const msoGarb = /<p\sclass="?MsoNormal"?[^]+?(?:"|')>/g; // is replaced by next line:
 		const msoClean = '<p class=default>';
 		const divGarb = /<div[^]*?>/g; // is deleted
-		temp = temp.replace(/(\n|\r|\f)/g, ' ');
-		temp = temp.replace(/\s\s/, ' ');
-		temp = temp.replace(styleGarb, '$1');
-		temp = temp.replace(msoGarb, msoClean);
-		temp = temp.replace(divGarb, '');
-		temp = temp.replace(doubleP, '');
+		chapHTML = chapHTML.replace(/(\n|\r|\f)/g, ' ');
+		chapHTML = chapHTML.replace(/\s\s/, ' ');
+		chapHTML = chapHTML.replace(styleGarb, '$1');
+		chapHTML = chapHTML.replace(msoGarb, msoClean);
+		chapHTML = chapHTML.replace(divGarb, '');
+		chapHTML = chapHTML.replace(doubleP, '');
 	};
 
 	ChapterHeadings();
@@ -30,23 +30,23 @@ function ReplaceText(){
 		const yearClean = '</h1><h2>$1</h2>'
 		const title = /<h1>([^]*?)<\/h1>[^]*?(<h2>[^]*?<\/h2>)[^]*?<p[^]*?\/p>[^]*?<p[^]*?>([^]*?)<\/p>/; //is replaced by:
 		const titleClean = '<h3>TITLE: $3</h3><h1>$1</h1>$2';
-		temp = temp.replace(chapHead, headClean);
-		temp = temp.replace(edYear, yearClean);
-		temp = temp.replace(title, titleClean);
+		chapHTML = chapHTML.replace(chapHead, headClean);
+		chapHTML = chapHTML.replace(edYear, yearClean);
+		chapHTML = chapHTML.replace(title, titleClean);
 	};
 
 	TableOfContents();
 	function TableOfContents(){
 		const tOC = new RegExp("(?<=<\\/h2>[^]*?)(<p[^>]*?>[^]*?<\\/p>)([^]*?)(?=\\1|<p class=default><b>)"); // is replaced by:
-		const repTOC = '<div id=toc><h2>Table of Contents</h2><div class=tocItems>$1$2</div></div><div class=orsbody><div class=temp>';
-		temp = temp.replace(tOC, repTOC);
+		const repTOC = '<div id=toc><h2>Table of Contents</h2><div class=tocItems>$1$2</div></div><div class=orsbody><div class=chapHTML>';
+		chapHTML = chapHTML.replace(tOC, repTOC);
 	};
 
 	ORSHighlight();
 	function ORSHighlight(){
 		const orsRef = /((\d{1,3}[A-C]?\.\d{3})((?:\s?(?:\(\w{1,4}\))){0,5})(\sto\s(?:(?:\(\w{1,4}\))))?)/g; // is replaced by:
 		const repORS = '<span class=ors>$1</span>';
-		temp = temp.replace(orsRef, repORS);
+		chapHTML = chapHTML.replace(orsRef, repORS);
 	};
 
 	Leadlines();
@@ -54,7 +54,7 @@ function ReplaceText(){
 		const orsLead = "(?:<span class=ors>)(" + chapNum + "\\.\\d{3})<\/span>([^\\.][^\\]]+?\\.)";
 		const orsHead = new RegExp("<p class=default><b>" + tabs + orsLead + "</b>",'g');
 		const repOrsH = '</div><div class=section id="$1" break="~"><p class=leadline>$1$2</p><p class=default>';
-		temp = temp.replace(orsHead, repOrsH);
+		chapHTML = chapHTML.replace(orsHead, repOrsH);
 	};
 
 	Forms();
@@ -65,51 +65,80 @@ function ReplaceText(){
 		const endForm = /(`([^~`]*_{78}|[^`~]*?(?=<div class=orsForm)))/g;
 		const repEndForm = "$1</div>";
 		const endFormClean =/_{78}(<\/div>)/g;
-		temp = temp.replace(startForm, repStartForm);
-		temp = temp.replace(endForm, repEndForm);
-		temp = temp.replace(endFormClean, '$1')
+		chapHTML = chapHTML.replace(startForm, repStartForm);
+		chapHTML = chapHTML.replace(endForm, repEndForm);
+		chapHTML = chapHTML.replace(endFormClean, '$1')
 	};
 
-	ORSHyperLinks();
-	function ORSHyperLinks(){
+	orsInChapLink();
+	function orsInChapLink(){
 		const inChap = new RegExp("<span class=ors(>(" + chapNum + "\\.\\d{3})[^]*?<\\/)span", 'g');
 		const repInChap = '<a class=ors href="#$2"$1a';
-		temp = temp.replace(inChap, repInChap);
+		chapHTML = chapHTML.replace(inChap, repInChap);
+	}
 
-		OutOfChapter();
-		function OutOfChapter(){
-			let oocCount = 0;
-			let tempReplace = "";
-			let repNotChap = "";
-			const orsURL = "https://www.oregonlegislature.gov/bills_laws/ors/ors";
-			const notChap = /=ors(>\d{1,3}[A-C]?\.\d{3})/;
-			const otherChap = /\d{1,3}[A-C]?(?=\.)/
+	orsOutChapLink();
+	function orsOutChapLink(){
+		let orsList =[];
+		const orsURL = "https://www.oregonlegislature.gov/bills_laws/ors/ors";
+		const notChap = /=ors>\d{1,3}[A-C]?\.\d{3}/g;
 
-			while (temp.match(notChap) && oocCount < 1000) {
-				oocCount +=1;
-				tempReplace = temp.match(notChap)[1];
-				tempReplace = tempReplace.match(otherChap);
-				tempReplace = ThreeDigit(tempReplace);
-				repNotChap = " a href='" + orsURL + tempReplace + ".html'";
-				temp = temp.replace(notChap, repNotChap);
-			};
-
-			function ThreeDigit(orsNumber){
-				orsNumber=orsNumber.toString();
-				let myLen = orsNumber.length;
-				if (orsNumber.match(/\d+[A-C]/)) {
-					myLen=myLen-1
-				};
-				switch(myLen) {
-					case 1:
-						return "00" + orsNumber;
-					case 2:
-						return "0" + orsNumber;
-					default:
-						return "" + orsNumber
-				};
+		//getting list of referenced ORS chapters not in chapter
+		let whileBreak = 0;
+		let continueLoop= true;
+		let temp=chapHTML;
+		while(continueLoop) {
+			if (whileBreak>500) {continueLoop=false};
+			strMatch=temp.match(notChap);
+			if (strMatch) {
+				console.log(strMatch);
+				chapNo=strMatch[0].match(/\d{1,3}[A-C]?/);
+				orsList.push(chapNo);
+				console.log(`${chapNo}`)
+				tempReplace = new RegExp('>'+chapNo +'\\.', 'g')
+				temp=temp.replace(tempReplace, "XXX");
+				whileBreak++;
+			} else {
+				continueLoop=false;
 			};
 		};
+		function onlyUnique(value, index, self) {
+			return self.indexOf(value) === index;
+		};
+		orsList = orsList.filter(onlyUnique);
+		
+		//replacing each reference to external chapter
+		
+		if (orsList.length>0){
+			for (let orsChap of orsList) {
+				if (orsChap.length<1) {break;};
+				let orsSearch = new RegExp("<span class=ors>("+orsChap+"\\.\\d{3}[^]*?)<\\/span>", 'g');
+				let urlChp = makeThreeDigit(orsChap);
+				let chpRep=`<a href="${orsURL}${urlChp}.html">$1</a>`;
+				chapHTML=chapHTML.replace(orsSearch, chpRep);
+			};
+		};
+		
+		function makeThreeDigit(orsNumber){
+			orsNumber=orsNumber.toString();  //ensuring ORS chapter number is string
+			let myLen = orsNumber.length;
+			if (orsNumber.match(/\d+[A-C]/)) {
+				myLen=myLen-1
+			};
+			switch(myLen) {
+				case 1:
+					return "00" + orsNumber;
+				case 2:
+					return "0" + orsNumber;
+				default:
+					return "" + orsNumber
+			};
+		};
+
+		let spanElements = document.body.getElementsByTagName('span')
+		for (let element of spanElements) {
+			element.tag
+		}
 	};
 
 	Notes();
@@ -122,10 +151,10 @@ function ReplaceText(){
 		const repPref = '<a href="https://www.oregonlegislature.gov/bills_laws/BillsLawsEDL/ORS_Preface.pdf">$1</a>';
 		const v22 = /(\d{4}\sComparative\sSection\sTable\slocated\sin\sVolume\s22)/g; // is replaced by:
 		const repV22 = '<a href="https://www.oregonlegislature.gov/bills_laws/Pages/ORS.aspx#">$1</a>';
-		temp = temp.replace(aNote, repNote);
-		temp = temp.replace(noteSec, repNS);
-		temp = temp.replace(preface, repPref);
-		temp = temp.replace(v22, repV22);
+		chapHTML = chapHTML.replace(aNote, repNote);
+		chapHTML = chapHTML.replace(noteSec, repNS);
+		chapHTML = chapHTML.replace(preface, repPref);
+		chapHTML = chapHTML.replace(v22, repV22);
 	}
 
 	SubUnits();
@@ -137,10 +166,10 @@ function ReplaceText(){
 		const repA = '<p class=subsec>$1</p>'
 		const repB = "<p class=subsubpara>$1</p>"
 		const repC = '<p class=subsubsubpara>$1</p>'
-		temp = temp.replace(Sub1, repA);
-		temp = temp.replace(aSub, repA);
-		temp = temp.replace(bPar, repB);
-		temp = temp.replace(cSPar, repC);
+		chapHTML = chapHTML.replace(Sub1, repA);
+		chapHTML = chapHTML.replace(aSub, repA);
+		chapHTML = chapHTML.replace(bPar, repB);
+		chapHTML = chapHTML.replace(cSPar, repC);
 
 		
 		Romans();
@@ -150,14 +179,14 @@ function ReplaceText(){
 	  // Lower Case:
 			const sameLetter = /=subsubpara>(\(([a-z])(?:\2){0,4}\))/g; // turn roman back into paragraphs if 1 letter or matching letters (aa)
 			const repSameL = "=para break='!'>$1";
-			temp = temp.replace(sameLetter, repSameL);
+			chapHTML = chapHTML.replace(sameLetter, repSameL);
 			const sameLetterC = /=subsubsubpara>(\(([A-Z])(?:\2){0,4}\))/g; // turn roman back into paragraphs if 1 letter or matching letters (aa)
 			const repSameLC = "=subpara break='?'>$1";
-			temp = temp.replace(sameLetterC, repSameLC);
+			chapHTML = chapHTML.replace(sameLetterC, repSameLC);
 
 			Roman_wrapper: {
 				breakIf = (romanNum) => {
-					Boolean(temp.match(`(${romanNum})`) == false)
+					Boolean(chapHTML.match(`(${romanNum})`) == false)
 				}
 				if(breakIf('ii')) {break Roman_wrapper}
 				const ii = /=para[^>]*>(\(i\)[^!~]*?)=para[^>]*>(?=\(ii\))/g; // next, get matching (i) & (ii) labeled as subsubs again
@@ -168,15 +197,15 @@ function ReplaceText(){
 				const v = new RegExp(leadRoman + "v)", 'g');
 				const x = new RegExp(leadRoman + "x)", 'g');
 				const xx = new RegExp(leadRoman + "xx)", 'g');
-				temp = temp.replace(ii, iiRep);
+				chapHTML = chapHTML.replace(ii, iiRep);
 				if(breakIf('iii')) {break Roman_wrapper}
-				temp = temp.replace(iii, repRoman);
+				chapHTML = chapHTML.replace(iii, repRoman);
 				if(breakIf('v')) {break Roman_wrapper}
-				temp = temp.replace(v, repRoman);
+				chapHTML = chapHTML.replace(v, repRoman);
 				if(breakIf('x')) {break Roman_wrapper}
-				temp = temp.replace(x, repRoman);
+				chapHTML = chapHTML.replace(x, repRoman);
 				if(breakIf('xx')) {break Roman_wrapper}
-				temp = temp.replace(xx, repRoman);
+				chapHTML = chapHTML.replace(xx, repRoman);
 		// Upper Case:
 				if(breakIf('II')) {break Roman_wrapper}
 				const iiC = /=subpara[^>]*>(\(I\)[^?~]*?)=subpara[^>]*>(?=\(II\))/g; // next, get matching (i) & (ii) labeled as subsubs again
@@ -187,15 +216,15 @@ function ReplaceText(){
 				const vC = new RegExp(leadRomanC + "V)", 'g');
 				const xC = new RegExp(leadRomanC + "X)", 'g');
 				const xxC = new RegExp(leadRomanC + "XX)", 'g');				
-				temp = temp.replace(iiC, iiRepC);
+				chapHTML = chapHTML.replace(iiC, iiRepC);
 				if(breakIf('III')) {break Roman_wrapper}
-				temp = temp.replace(iiiC, repRomanC);
+				chapHTML = chapHTML.replace(iiiC, repRomanC);
 				if(breakIf('V')) {break Roman_wrapper}
-				temp = temp.replace(vC, repRomanC);
+				chapHTML = chapHTML.replace(vC, repRomanC);
 				if(breakIf('X')) {break Roman_wrapper}
-				temp = temp.replace(xC, repRomanC);
+				chapHTML = chapHTML.replace(xC, repRomanC);
 				if(breakIf('XX')) {break Roman_wrapper}
-				temp = temp.replace(xxC, repRomanC);
+				chapHTML = chapHTML.replace(xxC, repRomanC);
 			}			
 		}
 
@@ -203,7 +232,7 @@ function ReplaceText(){
 		function LittleL() {
 			const littleL = /((=para[^>]*>\(k\)[^~!]*))(?:=subpara)[^>]*(?=>\(L\))/g
 			const repLL = "$1=para";
-			temp = temp.replace(littleL, repLL);
+			chapHTML = chapHTML.replace(littleL, repLL);
 		}
 	}
 
@@ -213,21 +242,21 @@ function ReplaceText(){
 		const repHead = '<p class=heading>$1</p>'
 		const subHead = /<p class=default>(\([^]{5,}?\))<\/p>/g //is replaced by:
 		const repSHead = '<p class=subhead>$1</p>'
-		temp = temp.replace(head, repHead);
-		temp = temp.replace(subHead, repSHead);
+		chapHTML = chapHTML.replace(head, repHead);
+		chapHTML = chapHTML.replace(subHead, repSHead);
 	}
 
 	SourceNotes();
 	function SourceNotes(){
 		const sNote = /(\[(19\d{2}|20\d{2}|Fo|Re|Am)[^]+?\]<\/p>)/g; //is replaced by:
 		const repSN = '<p class=sourceNote>$1</p>'
-		temp = temp.replace(sNote, repSN);
+		chapHTML = chapHTML.replace(sNote, repSN);
 
 		DeadORS();
 		function DeadORS(){
 			const deadOrs = /<p class=default><b>[^>\[]*?<a[^>\[]+?>([^<\[]+?)\s?<\/a>\s?<\/b>\s?<p class=sourceNote>/g
 			const repDeadO = "<p class='sourceNote leadline' id='$1'><b>$1</b>: "
-			temp = temp.replace(deadOrs, repDeadO);
+			chapHTML = chapHTML.replace(deadOrs, repDeadO);
 		}
 
 		HeinLinks();
@@ -238,14 +267,14 @@ function ReplaceText(){
 			const heinURL2 = "https://heinonline-org.soll.idm.oclc.org/HOL/SSLSearchCitation?journal=ssor&yearhi=$2&chapter=$1&sgo=Search&collection=ssl&search=go";
 			const orLaw2 =/(?:C|c)hapter\s(\d{1,4}),\sOregon\sLaws\s(\d{4})/g
 			const repOL2 ='<a href=' + heinURL2 + '>$&</a>';
-			temp = temp.replace(orLaw, repOL);
-			temp = temp.replace(orLaw2, repOL2);
+			chapHTML = chapHTML.replace(orLaw, repOL);
+			chapHTML = chapHTML.replace(orLaw2, repOL2);
 		}
 	}
 
 	FinalClean();
 	function FinalClean(){
-		document.body.innerHTML = temp;
+		document.body.innerHTML = chapHTML;
 		document.body.innerHTML = document.body.innerHTML.replace(doubleP, '');
 	}
 
@@ -266,7 +295,7 @@ function ReplaceText(){
 		document.body.appendChild(cssRefreshButton);	
 		document.body.appendChild(cssOffButton);
 	}
-}
+};
 
 function StyleSheetRefresh() {
 	chrome.runtime.sendMessage({message: "updateCSS"});  // sends message to background.js
@@ -277,4 +306,5 @@ function SendToConsole(htmlText, logText){
 	cleanUp = cleanUp.replace(/(<\/[^]+?>)/g, '$1\n');
 	console.log(logText);
 	console.log(cleanUp);
-}
+};
+
