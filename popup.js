18cae6ca7959960c1db0b9f6515de02254276059
RobertAtLaw <bobby.mauger@gmail.com>
2021-10-13 21:01:12 -0700

    Updates to option popup & data storage

--- popup.js
+++ popup.js
@@ -1,59 +1,72 @@
 //popup.js
-
 "use strict";
 
+//set global variables from popup.html form
+let darkSelector=document.getElementById("isDark");
+let orLawSelector=document.getElementById("OrLaws");
+let chpLaunchButton=document.getElementById("chapterLaunch");
+let darkTempValue="";
+let orLawTempValue="";
+
+//setup event listeners for form dropdowns & buttons
+darkSelector.addEventListener("change", ()=>{
+  if (darkTempValue!=darkSelector.value) {   // if new value selected
+    chrome.storage.sync.set({'isDarkStored': (darkSelector.value == 'Dark')})
+    displayUserOptions();
+  }
+});
+orLawSelector.addEventListener("change", ()=>{
+  if (orLawTempValue!=orLawSelector.value) {
+    chrome.storage.sync.set({'lawsReaderStored': orLawSelector.value}, reloadORS());
+    displayUserOptions();
+  };
+});
+chpLaunchButton.addEventListener("click", () => {
+  let orsChapter = "00"+document.getElementById("orsChapter").value
+  orsChapter = orsChapter.match(/\d{3}[A-C]?\b/)
+  chrome.tabs.create({url: 'https://www.oregonlegislature.gov/bills_laws/ors/ors' + orsChapter +'.html'});
+});
+// update displayed info at page launch & after dropdown changes:
+displayUserOptions();
 function displayUserOptions() {
+  darkTempValue=darkSelector;
+  orLawTempValue=orLawSelector;
   let promiseGetDark = new Promise((resolve, reject) => {
     chrome.storage.sync.get('isDarkStored', (object) => {
       if (object) {
-        resolve(object.isDarkStored)
-      } else {
-        reject(false);
-      }
+        resolve(object.isDarkStored && "Dark" || "Light")
+      } else {reject(false);}
     });
   })
   let promiseGetOrLaw = new Promise((resolve) => {
     chrome.storage.sync.get('lawsReaderStored', (object) => {
       if (object) {
         resolve(object.lawsReaderStored)
-      }
+      } else {reject(false);}
     });
   });
-  promiseGetDark.then((resolve) => {document.getElementById("isDark").checked = resolve});
-  promiseGetOrLaw.then((resolve) => {document.getElementById(resolve).checked=true;})
+  promiseGetDark.then((resolve) => {
+    for (let i=0; i<darkSelector.options.length; i++) {
+      if(darkSelector.options[i].value==resolve) {
+        darkSelector.selectedIndex=i;
+        break;
+      }
+    }
+  }).catch(console.log("error, user's stored dark/light status not found"));
+  promiseGetOrLaw.then((resolve) => {
+    for (let i=0; i<orLawSelector.options.length; i++) {
+      if(orLawSelector.options[i].value==resolve) {
+        orLawSelector.selectedIndex=i;
+        break;
+      }
+    }
+  }).catch("error - source of Ore Laws not retrie from popup.html formved from storages")
 };
 
-displayUserOptions();
-
-document.getElementById('optionsSubmit').onclick = () => {
-  chrome.storage.sync.set({'isDarkStored': document.getElementById("isDark").checked}) 
-  let orLaws="";
-  const orLawRadios = document.querySelectorAll('input[name="orLaws"]');
-  for (const choice of orLawRadios){
-    if (choice.checked) {
-      chrome.storage.sync.set({'lawsReaderStored': choice.id});
-      orLaws=choice.id;
+function reloadORS() {
+  chrome.tabs.query({url:'*://www.oregonlegislature.gov/bills_laws/ors/ors*.html'}, function(tabs) {
+    for (const aTab of tabs) {
+      chrome.tabs.reload(aTab.id)
     };
-  };
-};
-
-document.getElementById('chapterLaunch').onclick = () => {
-  let orsChapter = makeThreeDigit(document.getElementById("orsChapter").value);
-  chrome.tabs.create({url: 'https://www.oregonlegislature.gov/bills_laws/ors/ors' + orsChapter +'.html'});
-};
-
-function makeThreeDigit(orsNumber){
-  orsNumber=orsNumber.toString();  //ensuring ORS chapter number is string
-  let myLen = orsNumber.length;
-  if (orsNumber.match(/\d+[A-C]/)) {
-    myLen=myLen-1
-  };
-  switch(myLen) {
-    case 1:
-      return "00" + orsNumber;
-    case 2:
-      return "0" + orsNumber;
-    default:
-      return "" + orsNumber
-  };
+  });
 };
