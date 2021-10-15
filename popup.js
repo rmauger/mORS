741c3437d882490e61e230720e25a32af76ca4d9
Bobby Mauger <bobby.mauger@gmail.com>
2021-10-15 01:46:35 -0700

    Manual OrLaw lookup implemented in popup

--- popup.js
+++ popup.js
@@ -5,8 +5,31 @@
 let darkSelector=document.getElementById("isDark");
 let orLawSelector=document.getElementById("OrLaws");
 let chpLaunchButton=document.getElementById("chapterLaunch");
+let orLawsLaunchButton=document.getElementById("orLawsLaunch")
 let darkTempValue="";
 let orLawTempValue="";
+let orLawOrLegLookup={
+  "OL2021":"2021orlaw~.pdf",
+  "OL2020":"2020orlaw~.pdf",
+  "OL2019":"2019orlaw~.pdf",
+  "OL2018":"2018orlaw~.pdf",
+  "OL2017":"2017orlaw~.pdf",
+  "OL2016":"2016orlaw~.pdf",
+  "OL2015":"2015orlaw~.pdf",
+  "OL2014":"2014R1orLaw~ss.pdf",
+  "OL2013":"2013orlaw~.pdf",
+  "OL2012":"2012adv~ss.pdf",
+  "OL2011":"2011orLaw~.html",
+  "OL2010":"2010orLaw~.html",
+  "OL2009":"2009orLaw~.html",
+  "OL2008":"2008orLaw~.html",
+  "OL2007":"2007orLaw~.html",
+  "OL2006":"2006orLaw~ss1.pdf",
+  "OL2005":"2005orLaw~ses.html",
+  "OL2003":"2003orLaw~ses.html",
+  "OL2001":"2001orLaw~ses.html",
+  "OL1999":"1999orLaw~.html"
+}
 
 //setup event listeners for form dropdowns & buttons
 darkSelector.addEventListener("change", ()=>{
@@ -24,8 +47,38 @@ orLawSelector.addEventListener("change", ()=>{
 chpLaunchButton.addEventListener("click", () => {
   let orsChapter = "00"+document.getElementById("orsChapter").value
   orsChapter = orsChapter.match(/\d{3}[A-C]?\b/)
-  chrome.tabs.create({url: 'https://www.oregonlegislature.gov/bills_laws/ors/ors' + orsChapter +'.html'});
+  let orsURL = 'https://www.oregonlegislature.gov/bills_laws/ors/ors' + orsChapter +'.html'
+  chrome.tabs.create({url: orsURL});
 });
+orLawsLaunchButton.addEventListener("click", ()=>{  
+  let orLawsYear = document.getElementById("orLawsYear").value;
+  let orLawsChp = document.getElementById("orLawsChapter").value;
+  orLawSelector=document.getElementById("OrLaws");
+  let errMsg =""
+  let orLawURL=""
+  if ((orLawsYear > 1859 && orLawsYear < 2030)==false) {
+    errMsg +="Oregon Laws volume must be a year after 1859.\n";
+  } else if ((orLawsYear > 1999==false) && (orLawSelector.value=="OrLeg")) { 
+    errMsg +="Oregon Laws are not available on the Oregon Legislature\'s website for years before 1999.\n";
+  };
+  if ((orLawsChp < 2001 && orLawsChp > 0)==false) {
+    errMsg +="Chapter must be a number between 1 and 2000.\n"
+  }
+  if (orLawSelector.value=="None") {errMsg+="A session law lookup source (below) is required."}
+  if (errMsg.length>1) {
+    alert(errMsg)
+  } else { 
+    if (orLawSelector.value=="Hein"){
+      orLawURL = `https://heinonline-org.soll.idm.oclc.org/HOL/SSLSearchCitation?journal=ssor&yearhi=${orLawsYear}&chapter=${orLawsChp}&sgo=Search&collection=ssl&search=go` 
+    } else {
+      let orLawFileName = orLawOrLegLookup["OL"+orLawsYear].replace(/~/,"000"+orLawsChp)
+      orLawFileName=orLawFileName.replace(/([^]*?\w)\d*(\d{4}(?:\.|\w)*)/, "$1$2")
+      orLawURL = 'https://www.oregonlegislature.gov/bills_laws/lawsstatutes/'+orLawFileName
+    }
+    chrome.tabs.create({url: orLawURL})
+  };
+});
+
 // update displayed info at page launch & after dropdown changes:
 displayUserOptions();
 function displayUserOptions() {
